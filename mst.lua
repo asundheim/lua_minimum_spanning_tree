faf1eaf08d7bcf9881d605d5d9bf89da05b6a45a
Anders Sundheim <andersundheim@gmail.com>
2017-02-06 09:51:42 -0600

    Initial

---
+++ mst.lua
@@ -0,0 +1,41 @@
+local mst = {}
+function mst.tree(points,edges)
+  vertices = {points[1]}
+  tree = {}
+  while #vertices ~= #points do
+    ln = 10000
+    temp1 = nil
+    temp2 = nil
+    for i = 1, #vertices do
+      for p = 1, #points do
+        if doesexist(vertices,points[p])==false then
+            if dist(points[p][1],points[p][2],vertices[i][1],vertices[i][2])<ln and same(points[p][1],points[p][2],vertices[i][1],vertices[i][2],edges) then
+              ln = dist(points[p][1],points[p][2],vertices[i][1],vertices[i][2])
+              temp1 = points[p]
+              temp2 = vertices[i]
+            end
+          end
+        end
+      end
+      table.insert(vertices,temp1)
+      table.insert(tree,{temp1[1],temp1[2],temp2[1],temp2[2]})
+    end
+    for i = 1,#tree do
+      --print(tree[i][1],tree[i][2],tree[i][3],tree[i][4])
+    end
+    return tree
+end
+function dist(x1,y1,x2,y2)
+  return math.sqrt(math.pow(math.abs(x2-x1),2)+math.pow(math.abs(y2-y1),2))
+end
+
+function same(x1,y1,x2,y2,edg)
+  for i = 1,#edg do
+    if (x1==edg[i][1] and y1 == edg[i][1] and x2 == edg[i][3] and y2 == edg[i][4]) or (x2 == edg[i][1] and y2 == edg[i][2] and x1 == edg[i][3] and y1 == edg[i][4]) then
+      return true
+    end
+  end
+  return false
+end
+
+return mst
